e78dc07b040ce2abf1b90eebd2421b62ae7b8969
sebastian <segut10@hotmail.com>
2026-01-06 15:00:44 -0500

    agregando eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,15 @@
 import js from '@eslint/js';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
   {
     files: ['src/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
     rules: {
       'no-console': 'off',
     },
